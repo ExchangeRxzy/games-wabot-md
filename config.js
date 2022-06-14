be43da472eae7bbf6367e9180b1e051102b455d4
ExchangeRxzy <106664844+ExchangeRxzy@users.noreply.github.com>
2022-06-14 07:32:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281390658325'],
-  ['6282256115584'],
-  ['6281319944687', 'BG', true]
+  ['6283804678512'],
+  ['6288287037007'],
+  ['6288287037007', 'BG', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
